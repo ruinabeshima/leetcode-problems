7e524e60f20091907115d5f7bffd6e151b0dd9b7
Rui Nabeshima <147958900+ruinabeshima@users.noreply.github.com>
2026-04-20 08:51:33 +0900

    Time: 3 ms (51.09%) | Memory: 19.3 MB (68.27%) - LeetSync

--- 389-find-the-difference/find-the-difference.py
+++ 389-find-the-difference/find-the-difference.py
@@ -1,14 +1,3 @@
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
-        s_dict = {} 
-
-        for letter in s: 
-            s_dict[letter] = s_dict.get(letter, 0) + 1
-
-        for letter in t: 
-            if letter not in s_dict: 
-                return letter
-            elif s_dict[letter] == 0: 
-                return letter
-            else: 
-                s_dict[letter] -= 1 
+        return chr(sum(ord(letter) for letter in t) - sum(ord(letter) for letter in s))
