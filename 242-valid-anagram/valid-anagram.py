5f9ef99dc55ea04041337c3aed177111e74f5219
Rui Nabeshima <147958900+ruinabeshima@users.noreply.github.com>
2026-04-26 21:10:16 +0900

    Time: 11 ms (76.10%) | Memory: 19.4 MB (76.49%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,23 +1,22 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        letters_dict = {}
-
+        hashmap = {} 
+        
         for letter in s: 
-            if letter in letters_dict: 
-                letters_dict[letter] += 1 
+            if letter in hashmap: 
+                hashmap[letter] += 1 
             else: 
-                letters_dict[letter] = 1
-
+                hashmap[letter] = 1
 
         for letter in t: 
-            if letter not in letters_dict: 
+            if letter in hashmap: 
+                hashmap[letter] -= 1 
+            else: 
+                return False 
+        
+        for key in hashmap: 
+            if hashmap[key] != 0: 
                 return False 
 
-            if letter in letters_dict: 
-                letters_dict[letter] -= 1
-             
-        
-        for key in letters_dict: 
-            if letters_dict[key] != 0: 
-                return False 
         return True 
+
