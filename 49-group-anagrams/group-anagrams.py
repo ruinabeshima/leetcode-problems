6298d9b05857c801555e41a826a988a096552d2b
Rui Nabeshima <147958900+ruinabeshima@users.noreply.github.com>
2026-06-08 14:11:28 +0900

    Time: 18 ms (23.90%) | Memory: 23.9 MB (27.70%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,17 +1,12 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        hashmap = {} 
+        hashmap = defaultdict(list)
 
         for str in strs: 
             letters = [0] * 26 
 
             for letter in str: 
                 letters[ord(letter) - ord("a")] += 1 
+            hashmap[tuple(letters)].append(str)
 
-            if tuple(letters) in hashmap: 
-                hashmap[tuple(letters)].append(str)
-            else: 
-                hashmap[tuple(letters)] = [str] 
-
-        
-        return [hashmap[key] for key in hashmap]
+        return list(hashmap.values())
