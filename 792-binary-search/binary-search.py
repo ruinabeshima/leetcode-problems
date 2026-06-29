6c984fa39f0fc6e06efafc1306b53153f016dddd
Rui Nabeshima <147958900+ruinabeshima@users.noreply.github.com>
2026-06-29 13:05:00 +0900

    Time: 0 ms (100.00%) | Memory: 20.5 MB (74.52%) - LeetSync

--- 792-binary-search/binary-search.py
+++ 792-binary-search/binary-search.py
@@ -1,15 +1,15 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
         left = 0 
-        right = len(nums) -  1 
+        right = len(nums) - 1 
 
         while right >= left: 
-            complement = (right + left) // 2
+            complement = (left + right) // 2 
             if nums[complement] == target: 
-                return complement
-            elif nums[complement] > target: 
-                right = complement - 1 
+                return complement 
             elif nums[complement] < target: 
                 left = complement + 1 
-
+            else: 
+                right = complement - 1 
+        
         return -1
