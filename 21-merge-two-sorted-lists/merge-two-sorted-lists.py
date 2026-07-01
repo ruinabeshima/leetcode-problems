556ccdf1957c6eaaa565c5788c01dd4f923056a5
Rui Nabeshima <147958900+ruinabeshima@users.noreply.github.com>
2026-07-01 13:54:32 +0900

    Time: 0 ms (100.00%) | Memory: 19.3 MB (32.47%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.py
@@ -3,27 +3,28 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        dummy = ListNode(0) 
-        curr = dummy 
 
-        pointer_1 = list1 
-        pointer_2 = list2 
+        # Dummy pointer 
+        dummy = ListNode(0)
 
-        while pointer_1 and pointer_2: 
-            if pointer_1.val > pointer_2.val: 
-                curr.next = pointer_2 
-                pointer_2 = pointer_2.next 
+        ptr1, ptr2 = list1, list2 
+        curr = dummy
+        while ptr1 and ptr2: 
+            if ptr1.val <= ptr2.val: 
+                curr.next = ptr1 
+                ptr1 = ptr1.next
             else: 
-                curr.next = pointer_1 
-                pointer_1 = pointer_1.next 
+                curr.next = ptr2 
+                ptr2 = ptr2.next 
             curr = curr.next 
 
-        if pointer_1: 
-            curr.next = pointer_1 
-        elif pointer_2: 
-            curr.next = pointer_2 
-
-        return dummy.next 
+        if ptr1: 
+            curr.next = ptr1 
+        elif ptr2: 
+            curr.next = ptr2 
+        
+        return dummy.next
+            
+                
