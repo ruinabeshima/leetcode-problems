189a8a0ba97271098470e030351b1bd71fc06ec0
Rui Nabeshima <147958900+ruinabeshima@users.noreply.github.com>
2026-07-02 13:19:04 +0900

    Time: 3 ms (65.78%) | Memory: 18.8 MB (99.95%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.py
+++ 2-add-two-numbers/add-two-numbers.py
@@ -5,30 +5,35 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        def reverse(prev, curr): 
-            if curr is None: 
-                return prev 
-            temp = curr.next 
-            curr.next = prev 
-            return reverse(curr, temp)
-        
-        reverse_1 = reverse(None, l1)
-        reverse_2 = reverse(None, l2)
-        num1, num2 = "", "" 
+        curr = dummy = ListNode(0)
+        carry = 0 
 
-        while reverse_1: 
-            num1 += str(reverse_1.val)
-            reverse_1 = reverse_1.next
-        while reverse_2: 
-            num2 += str(reverse_2.val)
-            reverse_2 = reverse_2.next
-        result = int(num1) + int(num2)
-
-        dummy = ListNode(0)
-        curr = dummy
-        for i in range(len(str(result)) - 1, -1, -1): 
-            curr.next = ListNode(int(str(result)[i]))
+        while l1 or l2: 
+            if l1 and l2: 
+                l1val, l2val = l1.val, l2.val
+                l1 = l1.next
+                l2 = l2.next
+            elif not l1: 
+                l1val = 0 
+                l2val = l2.val 
+                l2 = l2.next
+            else: 
+                l1val = l1.val 
+                l2val = 0 
+                l1 = l1.next
+            
+            sum = l1val + l2val + carry
+            if sum < 10: 
+                curr.next = ListNode(sum)
+                carry = 0 
+            else: 
+                curr.next = ListNode(int(str(sum)[1]))
+                carry = int(str(sum)[0])
             curr = curr.next
-        return dummy.next
 
-        
+        if carry > 0: 
+            curr.next = ListNode(carry)
+
+        return dummy.next
+            
+            
