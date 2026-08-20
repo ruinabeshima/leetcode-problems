4040b430970e625b429e638404679caeac307b1a
Rui Nabeshima <147958900+ruinabeshima@users.noreply.github.com>
2026-08-20 09:31:45 +0900

    Time: 295 ms (6.88%) | Memory: 26.8 MB (21.45%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,30 +1,29 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        if not grid: 
-            return 0
-
-        rows, cols = len(grid), len(grid[0])
         islands = 0 
-        seen = set() 
+        seen = set()
+        rows, cols = len(grid), len(grid[0])
 
-        def bfs(r, c): 
+
+        def bfs(r, c):
+            directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
             q = collections.deque() 
-            seen.add((r, c)) 
-            q.append((r, c)) 
+            q.append((r, c))
+            seen.add((r, c))
 
-            while q: 
+            while q:
                 row, col = q.popleft() 
-                directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
-                for dr, dc in directions: 
+                for dr, dc in directions:
                     r, c = row + dr, col + dc 
-                    if (r in range(rows) and c in range(cols) and (r, c) not in seen and grid[r][c] == '1'): 
+                    if r in range(rows) and c in range(cols) and grid[r][c] == "1" and (r, c) not in seen:
                         seen.add((r, c))
-                        q.append((r, c))
+                        q.append((r,c))
+
 
         for r in range(rows): 
             for c in range(cols): 
-                if grid[r][c] == '1' and (r, c) not in seen: 
+                if grid[r][c] == "1" and (r, c) not in seen:
                     bfs(r, c)
-                    islands += 1 
+                    islands += 1
 
         return islands 
