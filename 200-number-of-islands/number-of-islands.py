3cb729107870badaf26b2d6d800a354bacb97df3
Rui Nabeshima <147958900+ruinabeshima@users.noreply.github.com>
2026-08-29 10:39:08 +0900

    Time: 291 ms (7.99%) | Memory: 31.6 MB (6.98%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -26,12 +26,24 @@ class Solution:
                         stack.append([r, c])
                         seen.add((r, c))
 
+        def iterative_bfs(row, col): 
+            queue = deque() 
+            queue.append([row, col])
+            seen.add((row, col))
+            directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
 
+            while queue: 
+                row, col = queue.popleft()
+                for dr, dc in directions: 
+                    r, c = row + dr, col + dc 
+                    if r in range(rows) and c in range(cols) and grid[r][c] == "1" and (r, c) not in seen:
+                        seen.add((r, c))
+                        queue.append([r, c])
 
         for row in range(rows): 
             for col in range(cols): 
                 if grid[row][col] == "1" and (row, col) not in seen: 
-                    iterative_dfs(row, col)
+                    iterative_bfs(row, col)
                     islands += 1 
         
         return islands 
