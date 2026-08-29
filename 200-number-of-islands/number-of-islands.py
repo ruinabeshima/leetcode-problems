c1f1ab1df0f78ae3aecf93a91795d804ce0fd3bc
Rui Nabeshima <147958900+ruinabeshima@users.noreply.github.com>
2026-08-29 10:36:41 +0900

    Time: 282 ms (10.81%) | Memory: 31.2 MB (6.98%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -13,10 +13,25 @@ class Solution:
                 if r in range(rows) and c in range(cols) and grid[r][c] == "1" and (r, c) not in seen: 
                     dfs(r, c)
 
+        def iterative_dfs(row, col): 
+            stack = [[row, col]]
+            seen.add((row, col))
+            directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+
+            while stack: 
+                row, col = stack.pop() 
+                for dr, dc in directions: 
+                    r, c = row + dr, col + dc
+                    if r in range(rows) and c in range(cols) and grid[r][c] == "1" and (r, c) not in seen: 
+                        stack.append([r, c])
+                        seen.add((r, c))
+
+
+
         for row in range(rows): 
             for col in range(cols): 
                 if grid[row][col] == "1" and (row, col) not in seen: 
-                    dfs(row, col)
+                    iterative_dfs(row, col)
                     islands += 1 
         
         return islands 
