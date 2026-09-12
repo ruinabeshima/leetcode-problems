1eef56a6c67bf250461631f5ad91b2638142a618
Rui Nabeshima <147958900+ruinabeshima@users.noreply.github.com>
2026-09-12 14:54:49 +0900

    Time: 218 ms (96.86%) | Memory: 21.7 MB (48.47%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,49 +1,24 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        rows, cols = len(grid), len(grid[0])
-        seen = set()
+        nrows, ncols = len(grid), len(grid[0])
         islands = 0 
 
-        def dfs(row, col): 
-            seen.add((row, col))
+        def dfs(r, c): 
+            if r < 0 or r >= nrows or c < 0 or c >= ncols: 
+                return 
+            if grid[r][c] == "0": 
+                return 
 
-            directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
-            for dr, dc in directions: 
-                r, c = row + dr, col + dc
-                if r in range(rows) and c in range(cols) and grid[r][c] == "1" and (r, c) not in seen: 
-                    dfs(r, c)
+            grid[r][c] = "0"
+            dfs(r + 1, c)
+            dfs(r - 1, c)
+            dfs(r, c + 1)
+            dfs(r, c - 1)
 
-        def iterative_dfs(row, col): 
-            stack = [[row, col]]
-            seen.add((row, col))
-            directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
-
-            while stack: 
-                row, col = stack.pop() 
-                for dr, dc in directions: 
-                    r, c = row + dr, col + dc
-                    if r in range(rows) and c in range(cols) and grid[r][c] == "1" and (r, c) not in seen: 
-                        stack.append([r, c])
-                        seen.add((r, c))
-
-        def iterative_bfs(row, col): 
-            queue = deque() 
-            queue.append([row, col])
-            seen.add((row, col))
-            directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
-
-            while queue: 
-                row, col = queue.popleft()
-                for dr, dc in directions: 
-                    r, c = row + dr, col + dc 
-                    if r in range(rows) and c in range(cols) and grid[r][c] == "1" and (r, c) not in seen:
-                        seen.add((r, c))
-                        queue.append([r, c])
-
-        for row in range(rows): 
-            for col in range(cols): 
-                if grid[row][col] == "1" and (row, col) not in seen: 
-                    iterative_bfs(row, col)
+        for i in range(nrows): 
+            for j in range(ncols): 
+                if grid[i][j] == "1": 
                     islands += 1 
-        
-        return islands 
+                    dfs(i, j)
+
+        return islands
